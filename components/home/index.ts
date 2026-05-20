bee4026e86863238d19c28154a61c73db67da1b8
Dev Jaja <jajak5242@gmail.com>
2026-05-20 06:56:57 -0400

    chore: update home barrel with all section exports

--- components/home/index.ts
+++ components/home/index.ts
@@ -1,4 +1,7 @@
 export { HeroSection } from "./HeroSection";
 export { FeaturesSection } from "./FeaturesSection";
 export { ProductsSection } from "./ProductsSection";
+export { HowItWorksSection } from "./HowItWorksSection";
+export { BlockchainCTA } from "./BlockchainCTA";
 export { TestimonialsSection } from "./TestimonialsSection";
+export { CTASection } from "./CTASection";
