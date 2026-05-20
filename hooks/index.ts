1b4558b9c34e07950a1daf984e8c20d304de4c8a
Dev Jaja <jajak5242@gmail.com>
2026-05-20 07:36:45 -0400

    chore: add useDebounce and useMediaQuery to hooks barrel

--- hooks/index.ts
+++ hooks/index.ts
@@ -3,3 +3,5 @@ export { useBatchCount } from "./useBatchCount";
 export { useRecentBatches } from "./useRecentBatches";
 export { useLocalStorage } from "./useLocalStorage";
 export { useSearchHistory } from "./useSearchHistory";
+export { useDebounce } from "./useDebounce";
+export { useMediaQuery } from "./useMediaQuery";
