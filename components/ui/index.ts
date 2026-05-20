1328bdd70d1b939947160432576a509406e873e9
Dev Jaja <jajak5242@gmail.com>
2026-05-20 07:32:06 -0400

    chore: add ProgressBar and StatCard to ui barrel

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -4,3 +4,5 @@ export { Button } from "./Button";
 export { Spinner } from "./Spinner";
 export { Tooltip } from "./Tooltip";
 export { CopyButton } from "./CopyButton";
+export { ProgressBar } from "./ProgressBar";
+export { StatCard } from "./StatCard";
