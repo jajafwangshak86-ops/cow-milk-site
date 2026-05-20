fbffb9b00a3b3eaeeb7b9ba3ef4018e4d80a12b4
Dev Jaja <jajak5242@gmail.com>
2026-05-20 07:38:51 -0400

    feat: add PartnersSection to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import {
-  HeroSection, TrustBanner, FeaturesSection, StatsSection, ProductsSection,
-  HowItWorksSection, BlockchainCTA, TestimonialsSection, FAQSection,
-  CTASection, NewsletterSection,
+  HeroSection, TrustBanner, PartnersSection, StatsSection, FeaturesSection,
+  ProductsSection, HowItWorksSection, BlockchainCTA, TestimonialsSection,
+  FAQSection, CTASection, NewsletterSection,
 } from "@/components/home";
 
 export default function Home() {
@@ -12,6 +12,7 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <TrustBanner />
+      <PartnersSection />
       <StatsSection />
       <FeaturesSection />
       <ProductsSection />
