28003761bc42074619e53c9f815ef5776ebca183
Dev Jaja <jajak5242@gmail.com>
2026-05-20 07:37:28 -0400

    chore: add StatsSection to home barrel

--- components/home/index.ts
+++ components/home/index.ts
@@ -8,3 +8,4 @@ export { CTASection } from "./CTASection";
 export { TrustBanner } from "./TrustBanner";
 export { NewsletterSection } from "./NewsletterSection";
 export { FAQSection } from "./FAQSection";
+export { StatsSection } from "./StatsSection";
