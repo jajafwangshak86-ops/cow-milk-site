f22266312de8ba0f78356a3281d0c03d6b5278f7
Dev Jaja <jajak5242@gmail.com>
2026-05-20 07:33:56 -0400

    chore: add new hooks to barrel

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,5 @@
 export { useBatch } from "./useBatch";
 export { useBatchCount } from "./useBatchCount";
 export { useRecentBatches } from "./useRecentBatches";
+export { useLocalStorage } from "./useLocalStorage";
+export { useSearchHistory } from "./useSearchHistory";
