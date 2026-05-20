773071c4052379793624d153f509b0dcf3f72095
Dev Jaja <jajak5242@gmail.com>
2026-05-20 07:31:24 -0400

    chore: add Tooltip and CopyButton to ui barrel

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,3 +2,5 @@ export { Badge } from "./Badge";
 export { Card } from "./Card";
 export { Button } from "./Button";
 export { Spinner } from "./Spinner";
+export { Tooltip } from "./Tooltip";
+export { CopyButton } from "./CopyButton";
