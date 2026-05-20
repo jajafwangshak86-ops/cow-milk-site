da22254d377b57d815e428a276b83f26ea793682
Dev Jaja <jajak5242@gmail.com>
2026-05-20 06:53:22 -0400

    chore: add barrel export for home components

---
+++ components/home/index.ts
@@ -0,0 +1,4 @@
+export { HeroSection } from "./HeroSection";
+export { FeaturesSection } from "./FeaturesSection";
+export { ProductsSection } from "./ProductsSection";
+export { TestimonialsSection } from "./TestimonialsSection";
